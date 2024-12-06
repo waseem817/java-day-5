5efa9ff5b3a04ba5adf422188ae2dd34260def84
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-06 15:06:57 +0530

    Add files via upload

---
+++ HallowSquare.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class HallowSquare
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new  Scanner(System.in);
+		int n=sc.nextInt();
+        for(int i=0; i<n; i++)
+         {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0||j==n-1||i==n-1||j==0)
+                System.out.print("*"+" ");
+                else
+                System.out.print(" "+" ");
+            }
+         
+            
+        System.out.println();
+        
+        
+        }
+	}
+}
